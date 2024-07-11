f1f535045bfa922b70600b45762a1802ca8f4900
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-07-11 19:42:41 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import App from './App';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Home from './routes/Home';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="Our-work" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);

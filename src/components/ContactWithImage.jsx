3a04a1b2f4dc72a8b07a094699d730289158cc1f
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-07-14 17:59:42 +0100

    images update 2

--- src/components/ContactWithImage.jsx
+++ src/components/ContactWithImage.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const ContactWithImage = () => {
   return (
-    <div className="bg-[url('/contact.jpg')] bg-cover bg-center w-screen h-[500px] md:h-[500px] mt-[120px] text-center flex flex-col items-center justify-center">
+    <div className="bg-[url('./contact.jpg')] bg-cover bg-center w-screen h-[500px] md:h-[500px] mt-[120px] text-center flex flex-col items-center justify-center">
       <div className="h-[250px] w-[400px] md:w-[500px] bg-[#0D7685] mt-10 justify-between items-center py-6 lg:py-10 px-4 lg:px-10 flex flex-col text-white">
         <h2 className="text-2xl font-medium"> Want To Know More ? </h2>
         <p className="font-light w-[80%] md:w-full">
@@ -14,7 +14,7 @@ const ContactWithImage = () => {
         <div className="flex text-center gap-10 justify-center items-center">
           <p className="font-light"> lasvegas@jpn-ent.net</p>
           <NavLink
-            to="./assets/contact"
+            to="/contact"
             className="bg-white hover:bg-transparent hover:text-white text-[#0D7685] font-semibold py-2 px-4 border-[#ffffff] border-2  "
           >
             Contact Us

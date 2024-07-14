fd6c607e0f44378e7daa16df5b83c064b6e7f24b
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-07-14 10:51:08 +0100

    deployment fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import Contact from './routes/Contact';
 import Home from './routes/Home';
 import OurWork from './routes/OurWork';
-
-import './index.css';
 import Services from './routes/Services';
+
+const repoName = '/jojodevadventure.github.io';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={repoName}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />

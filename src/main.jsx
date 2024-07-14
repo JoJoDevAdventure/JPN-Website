bf5a04eb78482396840c3e14f71353b29ad7c11c
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-07-14 18:33:19 +0100

    routes fix 3

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,14 @@ import OurWork from './routes/OurWork';
 import Services from './routes/Services';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router>
-      <Routes>
-        <Route path="/JPN-website/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/our-work" element={<OurWork />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route path="/JPN-website/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="our-work" element={<OurWork />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </Router>
 );

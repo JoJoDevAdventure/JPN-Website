36e1126f8f3e874b9cb7d934185cf86d8aeb83cd
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-07-31 12:02:58 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { HelmetProvider } from 'react-helmet-async';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
+    <HelmetProvider>
       <Outlet />
+    </HelmetProvider>
   );
 }
 
